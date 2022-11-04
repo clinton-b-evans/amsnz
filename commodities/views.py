d29ff5db0e9b8e31a014badbe398205cfdf2104f
safi786 <safiurrehmankhan4@gmail.com>
2022-11-04 21:51:20 +0500

    Transaction table fixes

--- commodities/views.py
+++ commodities/views.py
@@ -1,4 +1,5 @@
 import requests
+from django.db.models import Sum
 from django.shortcuts import render
 from django.http import HttpResponse
 from yahooquery import Ticker
@@ -148,75 +149,30 @@ def commodity_list_view(request, year):
     return render(request, "commodities/main.html", context)
 
 
-def commodities_latest_price_view(request, year):
-    commodities = Commodity.objects.filter(date__year=year)
-    transactions_obj = Transaction.objects.filter(date__year=year)
-    invested_total = 0
-    for commodity in commodities:
-        commodity.totalWeight = 0
-        commodity.invested = 0
-        transactions = Transaction.objects.filter(
-            commodity=commodity.pk, date__year=year
-        )
-        for transaction in transactions:
-            commodity.trans_ob = transaction
-            if transaction.transaction_type == "Buy":
-                commodity.totalWeight += transaction.weight
-                commodity.invested += transaction.value
-            if transaction.transaction_type == "Sell":
-                commodity.totalWeight -= transaction.weight
-                commodity.invested -= transaction.value
-        commodity.total = commodity.totalWeight * commodity.spot_price
-        invested_total += commodity.invested
-    grand_total = 0
+def commodity_transactions(request, year):
+    transactions = Transaction.objects.filter(date__year=year).annotate(score=Sum('value'))
 
-    for commodity in commodities:
-        grand_total += commodity.total
+    transactions_table = []
+    commodity_prices = get_commodities()
+    for transaction in transactions:
+        totalInvestment = float(transaction.weight) * float(transaction.value)
+        spotPrice = commodity_prices[transaction.commodity.commodity_class]
+        currentMarketValue = float(transaction.weight) * spotPrice
+        transactions_table.append({
+            "commodity": transaction.commodity,
+            "transaction_type": transaction.transaction_type,
+            "weight": transaction.weight,
+            "purchasedValue": transaction.value,
+            "date": transaction.date,
+            "totalInvestment": totalInvestment,
+            "spotPrice": spotPrice,
+            "currentMarketValue": currentMarketValue,
+            "profit_loss_percentage": ((currentMarketValue - totalInvestment) / totalInvestment) * 100
+        })
 
-    for commodity in commodities:
-        commodity.percentage = (commodity.total / grand_total) * 100
-        if commodity.percentage < 0:
-            commodity.percentage = 0.0
-
-    years = list(Transaction.objects.values_list("date__year").distinct())
-    years_list = []
-    for each in years:
-        for item in each:
-            years_list.append(item)
-
-    years_list = sort(years_list)
-    invested_total_year_wise = 0
-    commodities_invested_all_years_total_list = []
-
-    for my_year in years_list:
-        commodities_list = Commodity.objects.filter(date__year=my_year)
-        for item in commodities_list:
-            item.totalweight = 0
-            item.invested = 0
-            transaction_list = Transaction.objects.filter(
-                commodity=item.pk, date__year=my_year
-            )
-            for items in transaction_list:
-                if items.transaction_type == "Buy":
-                    item.totalweight += items.weight
-                    item.invested += items.value
-                if items.transaction_type == "Sell":
-                    item.totalweight -= items.weight
-                    item.invested -= items.value
-            item.total = item.totalweight * item.spot_price
-            invested_total_year_wise += item.invested
-        commodities_invested_all_years_total_list.append(
-            float(invested_total_year_wise)
-        )
     context = {
         "year": year,
-        "years_list": years_list,
-        "object_list": commodities,
-        "transactions_obj": transactions_obj,
-        "grand_total": grand_total,
-        "invested_total": invested_total,
-        "commodities_invested_all_years_total_list": commodities_invested_all_years_total_list,
-        "commodities_list": get_commodities(),
+        "transactions": transactions_table,
     }
     return render(request, "commodities/commodity_classes.html", context)
 
