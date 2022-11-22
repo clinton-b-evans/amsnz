32dbf347313d4f2f5428f5c9736cf1f9e20f2f4e
abdulqadeer273 <abdulqadeer@nexquery.com>
2022-11-22 21:15:23 +0500

    updations

--- incomestatements/views.py
+++ incomestatements/views.py
@@ -362,6 +362,8 @@ def year_to_date(request, year):
     net_income = []
     for key, value in month_expenses.items():
         net_income.append(month_income[key] - month_expenses[key])
+    print(total_expense, 'total_expense')
+    print(total_expense_budget, 'total_expense_budget')
     context = {
         "object_list": qs,
         "year": year,
