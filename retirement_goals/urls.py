59670e294f968d4d0eb5d69adea02db79ef60639
abdulqadeer273 <abdulqadeer@nexquery.com>
2022-12-01 16:07:48 +0500

    kindergarden and * is added

--- retirement_goals/urls.py
+++ retirement_goals/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import (
-    update_retirementgoal,
+    update_retirementgoal, add_retirement_goal,
 )
 
 app_name = "retirement_goals"
 
 urlpatterns = [
     path("retiregoals/", update_retirementgoal, name="update_retirementgoal"),
+    path("add_retirement_goal/", add_retirement_goal, name="add_retirement_goal"),
 ]
--- retirement_goals/views.py
+++ retirement_goals/views.py
@@ -1,9 +1,38 @@
+import json
+
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from retirement_goals.models import RetirementGoal
 from .forms import RetirementGoalForm
 import datetime
 from datetime import date
+
+
+def add_retirement_goal(request):
+    if request.method == "POST":
+        categoryData = json.loads(request.body)
+        obj = RetirementGoal.objects.create(
+            start_date=categoryData['start_date'],
+            networth_goal=categoryData["networth_goal"],
+            cpi=categoryData["cpi"],
+            real_estate=categoryData["real_estate"],
+            stocks=categoryData["stocks"],
+            crypto=categoryData["crypto"],
+            commodities=categoryData["commodities"],
+            collectables=categoryData["collectables"],
+            cash=categoryData["cash"],
+            real_estate_swr=categoryData["real_estate_swr"],
+            stocks_swr=categoryData["stocks_swr"],
+            user=request.user
+        )
+        user = {
+            'name': obj.networth_goal,
+        }
+        data = {
+            'user': user
+        }
+        print(data, 'data')
+        return JsonResponse(data)
 
 
 # Create your views here.
@@ -11,7 +40,7 @@ def update_retirementgoal(request):
     retirementgoal = RetirementGoal.objects.filter(user=request.user)
     if retirementgoal:
         for retirementgoal in retirementgoal:
-            print(retirementgoal,"retirementgoal")
+            print(retirementgoal, "retirementgoal")
             goal = retirementgoal.networth_goal
             start_year = retirementgoal.start_date.year
             current_year = date.today().year
@@ -59,7 +88,7 @@ def update_retirementgoal(request):
                 realstate_FV.append(real_estate_FV_value)
                 stocks_FV.append(stocks_FV_value)
                 amount_FV_value = (real_estate_FV_value * (real_estate_swr / 100)) + (
-                    stocks_FV_value * (stocks_swr / 100)
+                        stocks_FV_value * (stocks_swr / 100)
                 )
                 crypto_FV.append(int(current_goal * (crypto / 100)))
                 commodities_FV.append(int(current_goal * (commodities / 100)))
@@ -96,5 +125,6 @@ def update_retirementgoal(request):
             }
 
             return render(request, "retirementgoals/main.html", context)
+
     else:
         return render(request, "retirementgoals/main.html")
