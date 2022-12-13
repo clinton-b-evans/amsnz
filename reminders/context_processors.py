bf78831455a0287b7337672e2303e8aebc69ce7f
Safi - BESNARD CONSULTING <safi.rehman@dedomainia.com>
2022-12-13 21:25:25 +0500

    notification updated

--- reminders/context_processors.py
+++ reminders/context_processors.py
@@ -2,15 +2,22 @@ from reminders.models import Notification
 
 
 def user_notifications(request):
-    print(request.user, 'user')
-    notifications = Notification.objects.filter(reminder__user=request.user)
-    unread_notifications = Notification.objects.filter(reminder__user=request.user, mark_read=False)
-    unread = []
-    for i in range(1, len(unread_notifications)):
-        unread.append(unread_notifications[i])
+    if request.user:
+        notifications = Notification.objects.filter(reminder__user_id=request.user)
+        unread_notifications = Notification.objects.filter(reminder__user=request.user, mark_read=False)
+        unread = []
+        for i in range(1, len(unread_notifications)):
+            unread.append(unread_notifications[i])
 
-    return {
-        "unread": unread,
-        "notifications": notifications,
-        "length": len(notifications)
-    }
+        return {
+            "unread": unread,
+            "notifications": notifications,
+            "length": len(notifications)
+        }
+    else:
+        return {
+            "unread": [],
+            "notifications": [],
+            "length": 0
+        }
+
