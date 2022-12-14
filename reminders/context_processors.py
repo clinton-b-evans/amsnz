61059e32bc48b2d7f61cfe9c56cec0077be7da31
Safi - BESNARD CONSULTING <safi.rehman@dedomainia.com>
2022-12-14 12:02:17 +0500

    context processors fixed

--- reminders/context_processors.py
+++ reminders/context_processors.py
@@ -2,17 +2,23 @@ from reminders.models import Notification
 
 
 def user_notifications(request):
-    print(request.user, 'user')
-    unread_notifications = Notification.objects.filter(reminder__user=request.user, mark_read=False)
-    read_notifications = Notification.objects.filter(reminder__user=request.user, mark_read=True)
-    notifications = []
-    for i in range(0, len(unread_notifications)):
-        notifications.append(unread_notifications[i])
-    for i in range(0, len(read_notifications)):
-        notifications.append(read_notifications[i])
+    if not request.user.is_anonymous:
+        unread_notifications = Notification.objects.filter(reminder__user=request.user, mark_read=False)
+        read_notifications = Notification.objects.filter(reminder__user=request.user, mark_read=True)
+        notifications = []
+        for i in range(0, len(unread_notifications)):
+            notifications.append(unread_notifications[i])
+        for i in range(0, len(read_notifications)):
+            notifications.append(read_notifications[i])
 
-    return {
-        "unread": notifications,
-        "notifications": notifications,
-        "length": len(notifications)
-    }
+        return {
+            "unread": notifications,
+            "notifications": notifications,
+            "length": len(notifications)
+        }
+    else:
+        return {
+            "unread": [],
+            "notifications": [],
+            "length": 0
+        }
